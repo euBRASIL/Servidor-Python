fddaaaa371a24da41ff49383ca49b921c41e7926
Ricardo Costa <32866572+euBRASIL@users.noreply.github.com>
2023-08-10 17:37:42 -0300

    Update manage.py
    Instalação do ChatGPT

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import openai
 
 
 def main():
+    openai.organization = "Eletron-Bit"
+    openai.api_key = os.getenv("0549690c-63df-4f22-8b9f-6a3359734272")
+    openai.Model.list()
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
